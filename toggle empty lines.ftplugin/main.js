eaedae0fbe44eba5eab983a321712a505a7a8c12
Jesse Grosjean <jesse@hogbaysoftware.com>
2013-01-30 13:33:13 -0500

    Added plugin to toggle empty lines

---
+++ toggle empty lines.ftplugin/main.js
@@ -0,0 +1,26 @@
+define(function(require, exports, module) {
+	exports.editorDidLoad = function editorDidLoad(editor) {
+		editor.treeController.addCommand('toggle empty lines', 'Toggle between no empty lines and every other line empty.', function(treeController) {
+			var treeModel = treeController.treeModel,
+				undoManager = treeController.undoManager,
+				emptyLines = treeModel.evaluateNodePath('@type empty');
+											
+			undoManager.beginUndoGrouping();
+			treeModel.beginUpdates();
+			
+			if (emptyLines.count > 0) {
+				treeModel.removeNodes(emptyLines.nodesInLineOrder());
+			} else {
+				var each = treeModel.lineToNode(1);
+				while (each) {
+					treeModel.insertNodeBefore(treeModel.createNode(''), each);
+					each = each.nextLineNode();					
+				}				
+			}
+			
+			treeModel.endUpdates();
+			undoManager.endUndoGrouping();
+			undoManager.setActionName("Toggle Empty Lines");
+		});
+	};
+});
